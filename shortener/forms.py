1fc94617b813edf48a5b17a28ed53f3938fc09d5
Cine-Pixel <ikaumphriani@gmail.com>
2021-07-22 00:57:40 +0400

    gh-pages 📟

--- shortener/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms 
-
-
-class UrlForm(forms.Form):
-    url = forms.URLField(required=True, label="Url to shorten", widget=forms.URLInput(
-        attrs={'placeholder': "https://example.com/some/very/long/url?with=some&parameters=true"}
-    ))
--- smrl/settings/dev.py
+++
@@ -1,8 +0,0 @@
-from .common import *
-
-
-SECRET_KEY = 'django-insecure-8%c-0*8)ahei#sdhniz24fyzt)oq#7p-qn@zko3aqef^1wf00y'
-
-DEBUG = True
-
-ALLOWED_HOSTS = []
